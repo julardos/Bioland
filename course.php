9a93d1fdaf428a45e036916d12cfb677916e0662
Julardo <julardos@it.student.pens.ac.id>
2020-06-23 19:18:06 +0700

    add href in course

--- course.php
+++ course.php
@@ -7,15 +7,17 @@
                     <div class="row">
                         <?php if ($grade == 8) { ?>
                         <div class="col-3">
-                            <div class="card bg-skyblue border-0">
-                                <div class="course_img mx-auto">
-                                    <img src="<?php echo get_template_directory_uri() .
-                                        "/assets/peredaran-darah-bener.png" ?>"
-                                         class="img-fluid"
-                                         alt="">
+                            <a href="sistem-peredaran-darah">
+                                <div class="card bg-skyblue border-0">
+                                    <div class="course_img mx-auto">
+                                        <img src="<?php echo get_template_directory_uri() .
+                                            "/assets/peredaran-darah-bener.png" ?>"
+                                             class="img-fluid"
+                                             alt="">
+                                    </div>
+                                    <p class="text-center text-dark font-weight-bold">Sistem peredaran darah manusia</p>
                                 </div>
-                                <p class="text-center font-weight-bold">Sistem peredaran darah manusia</p>
-                            </div>
+                            </a>
                         </div>
                         <?php } else {?>
                         <div class="col-3">
