81ca25ed3afff62b0f996ae1b3b47fc594f29e0e
julardos <julardosatriawan@gmail.com>
2021-07-15 20:57:00 +0700

    h5p alatpernapasan

--- alatpernapasan.php
+++ alatpernapasan.php
@@ -9,13 +9,11 @@ get_header(); ?>
         <div class="container d-flex align-self-center mt-5 mt-lg-0 mt-md-0">
             <div class="row align-self-center w-100 mt-5 mt-lg-0 mt-md-0">
                 <div class="col-12 col-md-6 ">
-                    <img src="<?php echo get_template_directory_uri() . "/assets/pernafasan/alat-pernafasan-1.png" ?>" alt=""
-                         class="blood_flow img-fluid mx-auto">
+                    <iframe src="https://biolandclass.com/wp-admin/admin-ajax.php?action=h5p_embed&id=10" width="958" height="982" frameborder="0" allowfullscreen="allowfullscreen" title="Volume Udara Tidal"></iframe>
                 </div>
                 <div class="col-12 col-md-6 my-auto">
                     <h3 class="font-weight-bold">Alat Pernafasan</h3>
                     <p>Klik tanda "+" untuk mengetahui informasi bagian alat pernapasan lebih lanjut</p>
-
                 </div>
             </div>
         </div>
